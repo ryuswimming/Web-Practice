f6731aba8d04a016fed60d407d653600c04f645d
류수영 <ryuswimming@SwimmingsMacBookPro.local>
2024-02-10 23:33:30 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Web-Practice
+
+Web Develop Practice by 생활코딩
--- server.py
+++ server.py
@@ -2,85 +2,123 @@ from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
-nextId = 4
 
+nextId = 4
 topics = [
-    {'id' : 1, 'title': 'html', 'body' : 'html is ...'},
-    {'id' : 2, 'title': 'css', 'body' : 'css is ...'},
-    {'id' : 3, 'title': 'javascript', 'body' : 'javascript is ...'},
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
 ]
 
-def template(contents, content, id = None):
+
+def template(contents, content, id=None):
     contextUI = ''
     if id != None:
         contextUI = f'''
-            <li> <a href = "/update/{id}/"> update </a> </li>
+            <li><a href="/update/{id}/">update</a></li>
+            <li><form action="/delete/{id}/" method="POST"><input type="submit" value="delete"></form></li>
         '''
     return f'''<!doctype html>
     <html>
         <body>
-            <h1> <a href = '/'> WEB </a> </h1>
+            <h1><a href="/">WEB</a></h1>
             <ol>
                 {contents}
             </ol>
             {content}
             <ul>
-                <li> <a href = "/create/"> create </a> <li>
+                <li><a href="/create/">create</a></li>
                 {contextUI}
-            <ul>
+            </ul>
         </body>
     </html>
-'''
+    '''
+
 
 def getContents():
     liTags = ''
     for topic in topics:
-        liTags = liTags + f'<li> <a href = "/read/{topic["id"]}/"> {topic["title"]} </a> </li>'
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
     return liTags
+
 
 @app.route('/')
 def index():
     return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
 
-@app.route('/read/<id>/')
+
+@app.route('/read/<int:id>/')
 def read(id):
     title = ''
     body = ''
     for topic in topics:
-        if id == topic["id"]:
-            title = topic["title"]
-            body = topic["body"]
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
             break
-    return template(getContents(), f'<h2>{title}</h2>{body}', id = id)
+    return template(getContents(), f'<h2>{title}</h2>{body}', id)
 
-@app.route("/create/", methods = ["GET", "POST"])
+
+@app.route('/create/', methods=['GET', 'POST'])
 def create():
-    print("request.method", request.method)
-    if request.method == "GET":
+    if request.method == 'GET': 
         content = '''
-            <form action = "/create/" method = "POST">
-                <p> <input type = "text" name = "title" placeholder = "title"> </p>
-                <p> <textarea name = "body" placeholder = "body">  </textarea> </p>
-                <p> <input type = "submit" value = "create"> </p>
+            <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit" value="create"></p>
             </form>
         '''
         return template(getContents(), content)
-    elif request.method == "POST":
+    elif request.method == 'POST':
         global nextId
-        title = request.form["title"]
-        body = request.form["body"]
-        newTopic = {'id' : nextId, 'title': title, 'body' : body}
+        title = request.form['title']
+        body = request.form['body']
+        newTopic = {'id': nextId, 'title': title, 'body': body}
         topics.append(newTopic)
-        url = "/read/" + str(nextId) + '/'
-        nextId += 1
+        url = '/read/'+str(nextId)+'/'
+        nextId = nextId + 1
         return redirect(url)
 
-# method에 아무것도 넣지 않으면 기본으로 GET 요청이 되어서
-# submit을 눌렀을 때 action으로 지정된 경로에 들어가서 input 값을 ?parameter로 넣는다.
-# method에 POST로 요청을 넣으면 URL이 변하지 않은 채로 요청이 된다.
 
-# 정보를 읽어올 때는 GET으로 해서 URL에 정보가 묻어나오게 해도 되지만,
-# 정보를 변경할 때는 URL에 정보가 드러나지 않게 하는 것이 좋다.
-# 개발자 도구 -> payload에 그 은닉 정보가 들어가있다.
+@app.route('/update/<int:id>/', methods=['GET', 'POST'])
+def update(id):
+    if request.method == 'GET': 
+        title = ''
+        body = ''
+        for topic in topics:
+            if id == topic['id']:
+                title = topic['title']
+                body = topic['body']
+                break
+        content = f'''
+            <form action="/update/{id}/" method="POST">
+                <p><input type="text" name="title" placeholder="title" value="{title}"></p>
+                <p><textarea name="body" placeholder="body">{body}</textarea></p>
+                <p><input type="submit" value="update"></p>
+            </form>
+        '''
+        return template(getContents(), content)
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        for topic in topics:
+            if id == topic['id']:
+                topic['title'] = title
+                topic['body'] = body
+                break
+        url = '/read/'+str(id)+'/'
+        return redirect(url)
 
-app.run(port = 5001, debug = True)
+
+@app.route('/delete/<int:id>/', methods=['POST'])
+def delete(id):
+    for topic in topics:
+        if id == topic['id']:
+            topics.remove(topic)
+            break
+    return redirect('/')
+
+
+app.run(debug=True)
